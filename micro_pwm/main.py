140b1d1a9fe1bdd674290dc3f62dbc842f8f2f4b
alo40 <alejandronieto40@gmail.com>
2023-07-11 21:07:18 +0200

    added SLEEP_TIME constant

--- micro_pwm/main.py
+++ micro_pwm/main.py
@@ -4,6 +4,8 @@ import utime
 PWM_signal = machine.PWM(machine.Pin(16))
 PWM_signal.freq(1000)  # default 1000
 
+SLEEP_TIME = 0.0001
+
 max_duty_cycle = 65535
 
 # main routine
@@ -15,16 +17,16 @@ while True:
     # ramp up
     duty_cycle = 0
     while duty_cycle <= max_duty_cycle:
-        duty_cycle += 10
+        duty_cycle += 1
         PWM_signal.duty_u16(duty_cycle)
-        utime.sleep(0.001)
+        utime.sleep(SLEEP_TIME)
 
     # ramp down
     duty_cycle = max_duty_cycle
     while duty_cycle >= 0:
-        duty_cycle -= 10
+        duty_cycle -= 1
         PWM_signal.duty_u16(duty_cycle)
-        utime.sleep(0.001)
+        utime.sleep(SLEEP_TIME)
 
 # # end routine
 # PWM_signal.duty_u16(1)
